f402680aa461533a0a2a8c9eb95c53dcc5e5cb31
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-07 14:44:56 -0700

    it works

--- src/components/first.js
+++ src/components/first.js
@@ -1,33 +1,71 @@
 import React from 'react'
 
+
+const jikanBaseUrl = 'https://api.jikan.moe/v3/'
+
 class First extends React.Component {
     constructor() {
         super()
         this.state = {
-            something: 'sentence'
+            jikanUrl: '',
+            searchNameInput: '',
+            yourName: '',
         }
 
-        this.myFunction = this.myFunction.bind(this)
+        this.searchName = this.searchName.bind(this)
+        this.makeCall = this.makeCall.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
     }
 
-    myFunction() {
-        console.log('yes')
+    searchName(event) {
+        this.setState({
+            searchNameInput: event.target.value,
+            yourName: event.target.value,
+        }, () => {
+            console.log(this.state.yourName)
+        })
+    }
+
+    onSubmit() {
+        if(this.state.yourName !== '') {
+            this.setState({
+                jikanUrl: jikanBaseUrl + 'search/anime?q=' + this.state.yourName,
+            }, () => {
+                console.log(this.state.jikanUrl)
+                this.makeCall()
+            })
+        }
+    }
+
+    makeCall() {    
+        fetch(this.state.jikanUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+                this.setState({
+                    animeName: [data['results'][0]['title']],
+                }, () => {
+                    console.log(this.state.animeName)
+                })
+        })
     }
 
     render() {
-        this.myFunction()
         return(
             <div>
                 <div className="header">
                     <h1>Anime API</h1>
                 </div>
-                <div>
-                    <label>
-                        
-                    </label>
-                    <textarea>
-
-                    </textarea>
+                <div className="searchname">
+                    <label htmlFor="name">Search by name</label>
+                    <br/>
+                    <textarea name="name" value={this.state.searchNameInput} onChange={this.searchName} cols="50" rows="1"  placeholder="search by name" />
+                </div>
+                <div className="submitdiv">
+                    <button className="submitbutton" onClick={this.onSubmit}>Submit</button>
+                </div>
+                <div className="response">
+                    <p>{this.state.animeName}</p>
                 </div>
             </div>
         )

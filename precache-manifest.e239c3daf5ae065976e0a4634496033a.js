9da7eda8cf7470418d23894a1aa96b1404292446
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-07 13:30:34 -0700

    Updates

---
+++ precache-manifest.e239c3daf5ae065976e0a4634496033a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d25246ffd9d19ba4e712d1bace8d63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77bdcba86dbc8e293840",
+    "url": "./static/js/2.2b6f80e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2b6f80e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d7cef827b0304c0515",
+    "url": "./static/js/main.7c158e82.chunk.js"
+  },
+  {
+    "revision": "d1852769bb5ed6b18168",
+    "url": "./static/js/runtime-main.bc15de7f.js"
+  }
+]);

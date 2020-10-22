885d477f09a297404026246490c7b8154c2f7203
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-22 08:21:12 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e11b454b1412975e2cdca42da4b47270.js"
+  "./precache-manifest.5c272351ec1a7f3a7ed1c09859b5803c.js"
 );
 
 self.addEventListener('message', (event) => {
